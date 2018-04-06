66ff510b3c83471e5ce6a95d10bec0205ab6663b
Joan Bae <joanbae@Joan.local>
2018-04-06 11:20:30 +0900

    Footprints.ml 에 있던 모듈을 밖으로 빼냄

--- src/domain/Footprints.ml
+++ src/domain/Footprints.ml
@@ -1,32 +1,3 @@
-module Footprint =
-struct
-  type t = {
-    file: string; (** The analyzer's fileName **)
-    line: int;    (** Line number in that file **)
-    src_location: Cil.location; (** Describe a location in a C source file **)
-  }
-
-  let compare x y =
-    compare x.file y.file |> fun r -> if r <> 0 then r else
-      compare x.line y.line |> fun r -> if r <> 0 then r else
-        Cil.compareLoc x.src_location y.src_location
-
-  let pp fmt {file; line;
-              src_location = {Cil.file = src_file;
-                              line = src_line;
-                              byte = src_byte}} =
-    Format.fprintf fmt "%s:%d(%s:%d:%d)" file line src_file src_line src_byte
-
-  let to_string x =
-    pp Format.str_formatter x;
-    Format.flush_str_formatter ()
-
-  let of_here here src_location : t =
-    { file = here.Lexing.pos_fname;
-      line = here.Lexing.pos_lnum;
-      src_location}
-end
-
 include PowDom.MakeCPO (Footprint)
 
 let of_here here src_location = singleton (Footprint.of_here here src_location)
