6917ca30aea91c0411d9a2ad6e89257fd4e76220
joanbae <joanbae@snu.ac.kr>
2018-06-04 14:13:33 +0900

    [etc] modify gitignore and test example

--- test/pointer_minus.c
+++ test/pointer_minus.c
@@ -2,10 +2,12 @@
 
 int main()
 {
-char str[] = "Point";
-char *p1 = &str[0];
-char *p2 = &str[3];
-
-printf("result: %c",str[p2-p1]);
+  char str[] = "Point";
+  char *p1 = &str[0];
+  char *p2 = &str[3];
+  
+  char chr = str[(p2-p1)+100];
 return 0;
 }
+
+/*FN */
