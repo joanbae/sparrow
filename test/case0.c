1c57db0d3ccb0ad1fb93262a1c31306a7bdd3feb
joanbae <joanbae@snu.ac.kr>
2018-07-16 12:09:47 +0900

    example is added

---
+++ test/case0.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <dirent.h>
+#include <stdio.h>
+int main()
+{
+   DIR* dir_info;
+   struct dirent *dir_entry;
+   dir_info = opendir(".");
+   dir_entry = readdir(dir_info);
+   struct dirent *dir_entry2 = dir_entry + 10; //error를 내야하는데 안 내는 케이스
+   char *file_name =  dir_entry2 -> d_name;
+
+   
+   return 0;
+}
