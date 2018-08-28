32981ef36c0cf6eaa0a3a72cddf818a7619c160b
joanbae <joanbae@snu.ac.kr>
2018-08-28 17:00:14 +0900

    -bo 가 나도록 예제 변경

--- test/case0.c
+++ test/case0.c
@@ -7,7 +7,8 @@ int main()
    struct dirent *dir_entry, *dir_entry2;
    dir_info = opendir(".");
    dir_entry = readdir(dir_info);
-   dir_entry2 = dir_entry + 10;             //error를 내야하는데 안 내는 케이스
+   dir_entry2 = dir_entry + 500;
+
    char *file_name =  dir_entry2 -> d_name;
    
    return 0;
